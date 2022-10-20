3d6bc99f3b3e6393ae55eca237480b4a52124d3f
Ajithdomain <112495838+Ajithdomain@users.noreply.github.com>
2022-10-20 10:23:07 +0530

    Add files via upload

--- anagram.py
+++ anagram.py
@@ -1,23 +1,25 @@
-st=input().split()
+st=[str(i) for i in input().split()]
 L=[]
 m=[]
 t=[]
-for i in st:
-    l=[]
-    for j in st:
-        c=0
-        for k in i:
-            if(k in j):
-                c+=1
-            else:
-                break
+if(len(st)<=1):
+    L.append(st)
+    print(L)
+else:
+    for i in st:
+        l=[]
+        for j in st:
+            c=0
+            for k in i:
+                if(k in j):
+                    c+=1
             if(c==len(j)):
                 l.append(j)
-    L.append(l)
-    m.append(len(l))
-p=list(set(m))
+        L.append(l)
+        m.append(len(l))
+p=(set(m))
+print(L)
+print(m)
 for i in p:
     t.append(L[m.index(i)])
-print(t)
-            
-    
+print(t)                
