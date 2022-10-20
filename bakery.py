434fb18ec11a2b684bfd4cb46b339dc30bdcc171
Ajithdomain <112495838+Ajithdomain@users.noreply.github.com>
2022-10-20 10:01:03 +0530

    Add files via upload

--- bakery.py
+++ bakery.py
@@ -1,9 +1,5 @@
-new=int(input("enter number of fresh loaf:"))
-old=int(input("enter the number of old loaf:"))
-newr=new*185
-oldr=old*185
-newr=newr-oldr
-print("regular price:",float(185))
-print("amount of new loaf bread:",float(new*185))
-print("amount of day loaf bread:",newr*60/100)
-print("total amount:",(new*185)+(newr*60/100))
+n=int(input())
+o=int(input())
+print("regular price",185)
+print("purchased new loaf price",float(185*n))
+print("old loaf",((185*o)*60)/100)
--- dialpad.py
+++ dialpad.py
@@ -1,14 +1,27 @@
-n={2:"abc",3:"def",4:"ghi",5:"jkl",6:"mno",7:"pqrs",8:"tuv",9:"wxyz"}
-a=input()
+d={1:".",2:"abc",3:"def",4:"ghi",5:"jkl",6:"mno",7:"pqrs",8:"tuv",9:"wxyz",0:" "}
+n=[int(i)for i in input().split()]
 l=[]
-if(len(a)==1):
-    for i in range(len(n[int(a)])):
-        l.append(n[int(a)][i])
-else:
-    for i in range(len(n[int(a[0])])):
-        for j in range(len(n[int(a[1])])):
-            l.append(n[int(a[0])][i]+n[int(a[1])][j])
-print(l)
- 
- 
+k=[]
+s=""
+if(len(n)==1):
+    for i in range(len(n)-1):
+        if(n[i] in d):
+            s=d[n[i]]
+        k=[]
+    for i in range(len(s)):
+        for j in range(len(s)):
+            k.append(s[i]+" ")
+elif(len(n)==2):
+    for i in range(len(n)-1):
+        if(n[i] in d):
+            s=d[n[i]]
+    for i in range(1,len(n)):
+        if(n[i] in d):
+            m=d[n[i]]
+    for i in range(len(s)):
+        for j in range(len(m)):
+            k.append(s[i]+m[j])
+elif(n==" "):
+    print(l)
+print(k)
     
